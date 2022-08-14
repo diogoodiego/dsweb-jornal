aeecff17b5c22d63ce7d5800aac67981be207024
diogoodiego <diogo.sam.nascimento@gmail.com>
2022-08-13 23:24:49 -0300

    Revisão na funcões de login e registro de usuário

--- jornal/views.py
+++ jornal/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Usuario, Noticia, Edicao
 from django.utils import timezone
-from .forms import UserForm, UsuarioForm
+from .forms import UsuarioForm, LoginForm
 
 
 def index(request):
@@ -14,49 +14,56 @@ def index(request):
 
 class login_view(TemplateView):
     def get(self, request):
-        user_form = UserForm
+        login_form = LoginForm
         contexto ={
-            'user_form':user_form,
+            'login_form':login_form,
         }
         return render(request, 'jornal/login.html', contexto)
 
-    def post(self,request):
-        user_form = UserForm(request.POST)
-        if user_form.is_valid():
-            user = authenticate(request, email=user_form.email, password=user_form.password)
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            print(login_form.data['username'])
+            print(login_form.data['password'])
+            user = authenticate(request, username=login_form.data['username'], password=login_form.data['password'])
+            print(user)   
             if user is not None:
+                print('here')
                 login(request, user)
-                return redirect('jornal:index')
-            else:
-                messages.error(request, 'Email ou senha invalidos')
-                return redirect('jornal:login')
-        else:
-            contexto ={
-                'user_form':user_form,
-            }
-            return render(request, 'jornal/login.html', contexto)
+                return redirect('jornal:home')
+            
+        contexto ={
+            'login_form':login_form,
+        }
+        return render(request, 'jornal/login.html', contexto)
+
+
 
 class register_view(TemplateView):
     def get(self,request):
-        user_form = UserForm
         usuario_form = UsuarioForm
         contexto = {
-            'user_form':user_form,
             'usuario_form':usuario_form
         }
         return render(request,'jornal/register.html',contexto)
+
     def post(self, request):
-        user_form = UserForm(request.POST)
-        usuario_form = UsuarioForm(request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
+        usuario_form = UsuarioForm(request.POST, request.FILES)
+        if usuario_form.is_valid():
+            user = User.objects.create_user(username=usuario_form.data['username'], email=usuario_form.data['email'], password=usuario_form.data['password'])
+            user.save()
+            usuario = Usuario(
+                user = user,
+                tipo=usuario_form.data['tipo'],
+                imagem=usuario_form.files['imagem'],
+            )
+            usuario.save()
             return redirect('jornal:login')
-        else:
-            contexto = {
-                'user_form':user_form,
-                'usuario_form':usuario_form
-            }
-            return render(request,'jornal/register.html',contexto)
+
+        contexto = {
+            'usuario_form':usuario_form
+        }
+        return render(request,'jornal/register.html',contexto)
 
 def logout_view(request):
     logout(request)
