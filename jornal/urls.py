ea209818fd00801c762c05be5fe56d26ad9e308e
diogoodiego <diogo.sam.nascimento@gmail.com>
2022-08-13 23:22:35 -0300

    Habilitando urls

--- jornal/urls.py
+++ jornal/urls.py
@@ -10,10 +10,9 @@ urlpatterns = [
     path('login/',views.login_view.as_view(), name='login'),
     path('register',views.register_view.as_view(), name='register'),
     path('logout',views.logout_view, name='logout'),
-
-    #path('home',views.home.as_view(), name='home'),
-    #path('noticia',views.noticia_view.as_view(), name='noticia'),
-    #path('noticia/<int:noticia_id>/', views.noticia_detalhe.as_view(), name='noticia_detalhe')
+    path('home',views.home.as_view(), name='home'),
+    path('noticia',views.noticia_view.as_view(), name='noticia'),
+    path('noticia/<int:noticia_id>/', views.noticia_detalhe.as_view(), name='noticia_detalhe')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
