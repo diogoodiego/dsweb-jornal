4f60a2b5a92282dd525642f76cb92129782d200d
diogoodiego <diogo.sam.nascimento@gmail.com>
2022-08-13 23:23:20 -0300

    Adicionando forms

--- jornal/forms.py
+++ jornal/forms.py
@@ -1,27 +1,29 @@
+import email
 from django import forms
-from django.forms import ModelForm, Textarea, TextInput, Select
+from django.forms import ModelForm, Textarea, TextInput, Select, ImageField, ChoiceField
 from django.contrib.auth.models import User
 from .models import Usuario
 
-#class UserForm(forms.Form):
-#    username = forms.CharField(max_length=200)
-#    email = forms.CharField(max_length=200)
-#    password = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'class': 'form-control form-control-sm','type':'password'}))
+TIPO_CHOICES = (
+    ('e','Editor'),
+    ('l','Leitor')
+)
 
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ('username','email','password')
-        widgets = {
-            'username': TextInput(attrs={'class': 'form-control form-control-sm'}),
-            'email': TextInput(attrs={'class': 'form-control form-control-sm','type':'email'}),
-            'password': TextInput(attrs={'class': 'form-control form-control-sm', 'type': 'password'}),
-        }
+class UsuarioForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    email = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150)
+    tipo = forms.ChoiceField(choices=TIPO_CHOICES)
+    imagem = ImageField()
 
-class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model = Usuario
-        fields = '__all__'
-        widgets = {
-            'tipo' : Select(attrs={'class':'form-select'}),
-        }
+    username.widget.attrs.update({'class': 'form-control'})
+    email.widget.attrs.update({'class': 'form-control'})
+    password.widget.attrs.update({'class': 'form-control'})
+    tipo.widget.attrs.update({'class': 'form-select'})
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150)
+
+    username.widget.attrs.update({'class': 'form-control'})
+    password.widget.attrs.update({'class': 'form-control'})
